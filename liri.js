9433be34d97b01b2cbf201af74d40ecebfb8046a
Brian Taylor Johnson <brian.johnson@cigna.com>
2019-07-30 00:58:16 -0400

    Added logic for using Spotify to search song names and return data about the song including a preview URL

---
+++ liri.js
@@ -0,0 +1,56 @@
+require("dotenv").config();
+var Spotify = require('node-spotify-api');
+var util = require("util");
+
+var keys = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
+var operation = process.argv[2];
+var argumentOne = process.argv[3];
+
+if (operation === "concert-this")
+{
+    console.log("Concert this!");
+}
+
+else if (operation === "spotify-this-song")
+{
+    console.log("Spotify this song!");
+    spotify
+        .search({ type: 'track', query: argumentOne, limit: 1 })
+        .then(function(response)
+        {
+            var track = response.tracks.items[0].name;
+            var artist = response.tracks.items[0].artists[0].name;
+            var previewUrl = response.tracks.items[0].preview_url;
+            var album = response.tracks.items[0].album.name;
+
+
+            console.log("");
+            console.log("<><><><><><><> Spotify A Song <><><><><><><>");
+            console.log("Artist(s): " + artist);
+            console.log("Song Name: " + track);
+            console.log("Preview Link: " + previewUrl);
+            console.log("Album: " + album);
+            console.log("<><><><><><><><><><><><><><><><><><><><><><>");
+            console.log("");
+        })
+        .catch(function(err)
+        {
+            console.log("Whoops! Looks like something went wrong. Please try again.");
+    });
+}
+
+else if (operation === "movie-this")
+{
+    console.log("Movie this!");
+}
+
+else if (operation === "do-what-it-says")
+{
+    console.log("Do what it says!");
+}
+
+else
+{
+    console.log("Pleaes enter a valid command line operation.");
+}
