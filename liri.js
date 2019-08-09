a43a3c57918157a1f47781d00e94c83c4a7ff5e7
Brian Taylor Johnson <brian.johnson@cigna.com>
2019-08-08 22:03:15 -0400

    Added logic for Do What It Says to run from text file

--- liri.js
+++ liri.js
@@ -2,14 +2,14 @@ require("dotenv").config();
 var inquirer = require("inquirer");
 const axios = require('axios');
 var moment = require("moment");
+var fs = require('fs');
 
 var Spotify = require('node-spotify-api');
 var util = require("util");
 
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
-var operation;
-//var argumentOne = process.argv[3];
+
 var username = "";
 var task = "";
 
@@ -82,8 +82,9 @@ function pickTask()
     });
 }
 
-function somethingElse()
+function somethingElse(text)
 {
+    
     inquirer.prompt(
     [
         {
@@ -106,135 +107,284 @@ function somethingElse()
     });
 }
 
-function concertThis()
+function concertThis(value)
 {
-    inquirer.prompt(
-    [
-        {
-            name: "band",
-            message: "Which artist/band would you like to see concert info on?",
-            validate: function(value)
-            {
-                if(value == "")
-                {
-                    return false;
-                }
-                return true;
-            }
-        }
-    ]).then(function(response)
+    switch(value)
     {
-        var band = response.band;
-
-        axios.get("https://rest.bandsintown.com/artists/" + band + "/events?app_id=codingbootcamp")
-        .then(function(response)
-        {
+        case undefined:
+            inquirer.prompt(
+                [
+                    {
+                        name: "band",
+                        message: "Which artist/band would you like to see concert info on?",
+                        validate: function(value)
+                        {
+                            if(value == "")
+                            {
+                                return false;
+                            }
+                            return true;
+                        }
+                    }
+                ]).then(function(response)
+                {
+                    var band = response.band;
             
-
+                    axios.get("https://rest.bandsintown.com/artists/" + band + "/events?app_id=codingbootcamp")
+                    .then(function(response)
+                    {
+                        
             
-            for(i = 0; i < 5; i++)
-            {
-                console.log("Venue: " + response.data[i].venue.name);
-                console.log("Location: " + response.data[i].venue.city + ", " + response.data[i].venue.region + ", " + response.data[i].venue.country);
-                console.log("Date: " + moment(response.data[i].datetime).format('MMMM DD YYYY, h:mm:ss a') + "\n");
-            }
-            somethingElse();
-        })
-        .catch(function(error)
-        {
-            console.log("Uh oh! Something went wrong. I promise it's not you. It's me. Try again later.");
-            somethingElse();
-        });
-    });
+                        
+                        for(i = 0; i < 5; i++)
+                        {
+                            console.log("Venue: " + response.data[i].venue.name);
+                            console.log("Location: " + response.data[i].venue.city + ", " + response.data[i].venue.region + ", " + response.data[i].venue.country);
+                            console.log("Date: " + moment(response.data[i].datetime).format('MMMM DD YYYY, h:mm:ss a') + "\n");
+                        }
+                        somethingElse();
+                    })
+                    .catch(function(error)
+                    {
+                        console.log("Uh oh! Something went wrong. I promise it's not you. It's me. I may not have found the artist/band you entered. Please try.");
+                        somethingElse();
+                    });
+                });
+        break;
+
+        default:
+                var band = value;
+            
+                axios.get("https://rest.bandsintown.com/artists/" + band + "/events?app_id=codingbootcamp")
+                .then(function(response)
+                {
+                    for(i = 0; i < 5; i++)
+                    {
+                        console.log("Venue: " + response.data[i].venue.name);
+                        console.log("Location: " + response.data[i].venue.city + ", " + response.data[i].venue.region + ", " + response.data[i].venue.country);
+                        console.log("Date: " + moment(response.data[i].datetime).format('MMMM DD YYYY, h:mm:ss a') + "\n");
+                    }
+                    somethingElse();
+                })
+                .catch(function(error)
+                {
+                    console.log("Uh oh! Something went wrong. I promise it's not you. It's me. I may not have found the artist/band you entered. Please try.");
+                    somethingElse();
+                });
+            break;
+    }
 }
 
-function spotifySong()
+function spotifySong(value)
 {
     var song;
 
-    inquirer.prompt(
-    [
-        {
-            name: "song",
-            message: "What song title would you like to look up on Spotify?"
-        }
-    ]).then(function(response)
+    switch(value)
     {
-        song = response.song;
-        
-        if(song !== "")
-        {
-            spotify
-            .search({ type: 'track', query: song, limit: 1 })
-            .then(function(response)
-            {
-                var track = response.tracks.items[0].name;
-                var artist = response.tracks.items[0].artists[0].name;
-                var previewUrl = response.tracks.items[0].preview_url;
-                var album = response.tracks.items[0].album.name;
-
-                if (previewUrl == null)
+        case undefined:
+            inquirer.prompt(
+            [
                 {
-                    previewUrl = "No preview available at this time.";
+                    name: "song",
+                    message: "What song title would you like to look up on Spotify?"
                 }
-                
-                console.log("");
-                console.log("<><><><><><><> Spotify A Song <><><><><><><>");
-                console.log("Artist(s): " + artist);
-                console.log("Song Name: " + track);
-                console.log("Preview Link: " + previewUrl);
-                console.log("Album: " + album);
-                console.log("<><><><><><><><><><><><><><><><><><><><><><>");
-                console.log("");
-
-                somethingElse();
-            })
-            .catch(function(err)
+            ]).then(function(response)
             {
-                console.log("Uh oh! Something went wrong. I promise it's not you. It's me. Try again later.");
-                somethingElse();
+                song = response.song;
+                
+                if(song !== "")
+                {
+                    spotify
+                    .search({ type: 'track', query: song, limit: 1 })
+                    .then(function(response)
+                    {
+                        var track = response.tracks.items[0].name;
+                        var artist = response.tracks.items[0].artists[0].name;
+                        var previewUrl = response.tracks.items[0].preview_url;
+                        var album = response.tracks.items[0].album.name;
+        
+                        if (previewUrl == null)
+                        {
+                            previewUrl = "No preview available at this time.";
+                        }
+                        
+                        console.log("");
+                        console.log("<><><><><><><> Spotify A Song <><><><><><><>");
+                        console.log("Artist(s): " + artist);
+                        console.log("Song Name: " + track);
+                        console.log("Preview Link: " + previewUrl);
+                        console.log("Album: " + album);
+                        console.log("<><><><><><><><><><><><><><><><><><><><><><>");
+                        console.log("");
+        
+                        somethingElse();
+                    })
+                    .catch(function(err)
+                    {
+                        console.log("Uh oh! Something went wrong. I promise it's not you. It's me. I may not have found the song you are looking for. Please, try again.");
+                        somethingElse();
+                    });
+                }
+                else
+                {
+                    console.log("\n<><><><><><><> Spotify A Song <><><><><><><>");
+                    console.log("Artist(s): Ace of Base");
+                    console.log("Song Name: The Sign");
+                    console.log("Preview Link: https://p.scdn.co/mp3-preview/4c463359f67dd3546db7294d236dd0ae991882ff?cid=f36a84e4bc524e7692764fdaf45eb206");
+                    console.log("Album: The Sign (US Album) [Remastered]");
+                    console.log("<><><><><><><><><><><><><><><><><><><><><><>");
+                    console.log("");
+        
+                    somethingElse();
+                }
             });
-        }
-        else
-        {
-            console.log("\n<><><><><><><> Spotify A Song <><><><><><><>");
-            console.log("Artist(s): Ace of Base");
-            console.log("Song Name: The Sign");
-            console.log("Preview Link: https://p.scdn.co/mp3-preview/4c463359f67dd3546db7294d236dd0ae991882ff?cid=f36a84e4bc524e7692764fdaf45eb206");
-            console.log("Album: The Sign (US Album) [Remastered]");
-            console.log("<><><><><><><><><><><><><><><><><><><><><><>");
-            console.log("");
-
-            somethingElse();
-        }
-    }); 
+            break
+        
+        default:
+                song = value;
+                
+                if(song !== "")
+                {
+                    spotify
+                    .search({ type: 'track', query: song, limit: 1 })
+                    .then(function(response)
+                    {
+                        var track = response.tracks.items[0].name;
+                        var artist = response.tracks.items[0].artists[0].name;
+                        var previewUrl = response.tracks.items[0].preview_url;
+                        var album = response.tracks.items[0].album.name;
+        
+                        if (previewUrl == null)
+                        {
+                            previewUrl = "No preview available at this time.";
+                        }
+                        
+                        console.log("");
+                        console.log("<><><><><><><> Spotify A Song <><><><><><><>");
+                        console.log("Artist(s): " + artist);
+                        console.log("Song Name: " + track);
+                        console.log("Preview Link: " + previewUrl);
+                        console.log("Album: " + album);
+                        console.log("<><><><><><><><><><><><><><><><><><><><><><>");
+                        console.log("");
+        
+                        somethingElse();
+                    })
+                    .catch(function(err)
+                    {
+                        console.log("Uh oh! Something went wrong. I promise it's not you. It's me. I may not have found the song you are looking for. Please, try again.");
+                        somethingElse();
+                    });
+                }
+                else
+                {
+                    console.log("\n<><><><><><><> Spotify A Song <><><><><><><>");
+                    console.log("Artist(s): Ace of Base");
+                    console.log("Song Name: The Sign");
+                    console.log("Preview Link: https://p.scdn.co/mp3-preview/4c463359f67dd3546db7294d236dd0ae991882ff?cid=f36a84e4bc524e7692764fdaf45eb206");
+                    console.log("Album: The Sign (US Album) [Remastered]");
+                    console.log("<><><><><><><><><><><><><><><><><><><><><><>");
+                    console.log("");
+        
+                    somethingElse();
+                }
+            break;
+    }
+    
 }
 
-function movieThis()
+function movieThis(value)
 {
-    console.log("Movie this!");
-
-    axios.get("http://www.omdbapi.com/?apikey=1a62d00c&t=Forrest+Gump")
-    .then(function(response) {
-
-        console.log("Title: " + response.data.Title);
-        console.log("Year: " + response.data.Year);
-        console.log("IMDB Rating: " + response.data.Ratings[0].Value);
-        console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
-        console.log("Country: " + response.data.Country);
-        console.log("Language: " + response.data.Language);
-        console.log("Plot: " + response.data.Plot);
-        console.log("Actors: " + response.data.Actors);
-    })
-    .catch(function(error)
+    switch(value)
     {
-        console.log(error);
-    });
+        case undefined:
+                inquirer.prompt(
+                    [
+                        {
+                            name: "movie",
+                            message: "Which movie would you like to see info on?",
+                            validate: function(value)
+                            {
+                                if(value == "")
+                                {
+                                    return false;
+                                }
+                                    return true;
+                                }
+                        }
+                        ]).then(function(response)
+                        {
+                            var movie = response.movie;
+                
+                            axios.get("http://www.omdbapi.com/?apikey=1a62d00c&t=" + movie)
+                            .then(function(response) {
+                            console.log("Title: " + response.data.Title);
+                            console.log("Year: " + response.data.Year);
+                            console.log("IMDB Rating: " + response.data.Ratings[0].Value);
+                            console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+                            console.log("Country: " + response.data.Country);
+                            console.log("Language: " + response.data.Language);
+                            console.log("Plot: " + response.data.Plot);
+                            console.log("Actors: " + response.data.Actors + "\n");
+                            somethingElse();
+                        })
+                        .catch(function(error)
+                        {
+                            console.log("Uh oh! Something went wrong. I promise it's not you. It's me. I may not have found the movie you are looking for. Please, try again.");
+                            somethingElse();
+                        });
+                        });
+            break
+
+        default:
+                var movie = value;
+                
+                axios.get("http://www.omdbapi.com/?apikey=1a62d00c&t=" + movie)
+                .then(function(response) {
+                console.log("Title: " + response.data.Title);
+                console.log("Year: " + response.data.Year);
+                console.log("IMDB Rating: " + response.data.Ratings[0].Value);
+                console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+                console.log("Country: " + response.data.Country);
+                console.log("Language: " + response.data.Language);
+                console.log("Plot: " + response.data.Plot);
+                console.log("Actors: " + response.data.Actors + "\n");
+                somethingElse();
+            })
+            .catch(function(error)
+            {
+                console.log("Uh oh! Something went wrong. I promise it's not you. It's me. I may not have found the movie you are looking for. Please, try again.");
+                somethingElse();
+            });
+    }
 }
 
 function doIt()
 {
-    console.log("Do what it says!");
+    var text = [];
+    var command = "";
+    var query = "";
+    
+    fs.readFile('random.txt', 'utf8', function(err, contents) {
+        text = contents.split(",");
+        command = text[0].substring(0, text[0].length);
+        query = text[1].substring(1, (text[1].length - 1));
+
+        switch(command)
+        {
+            case "concert-this":
+                concertThis(query);
+                break
+
+            case "spotify-this-song":
+                spotifySong(query);
+                break
+
+            case "movie-this":
+                movieThis(query);
+                break
+        }
+    });
 }
 
 loginUser();
